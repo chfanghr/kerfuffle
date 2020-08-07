dad9d7616d41401adcac6236c8c0adb5c623f3e3
方泓睿 <chfanghr@gmail.com>
2020-08-07 18:53:54 +0800

    ci: rename build script directory

--- tools/build/msvc-x86_64-windows-release.cmd
+++
@@ -1,6 +0,0 @@
-MKDIR cmake-msvc-x86_64-windows-build-release
-conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s build_type=Release
-CD cmake-msvc-x86_64-windows-build-release
-cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
-msbuild .\ALL_BUILD.vcxproj
-ctest -C "Release"
