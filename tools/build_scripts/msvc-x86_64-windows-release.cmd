d98beb286a94b749d42bb049e8df43540cabfed8
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2020-08-07 22:05:50 +0800

    ci: using build type DebugAndRelease(try: 5)

--- tools/build_scripts/msvc-x86_64-windows-release.cmd
+++ tools/build_scripts/msvc-x86_64-windows-release.cmd
@@ -1,6 +1,6 @@
 MKDIR cmake-msvc-x86_64-windows-build-release
-conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s build_type=RelWithDebInfo --build
+conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s build_type=DebugAndRelease 
 CD cmake-msvc-x86_64-windows-build-release
-cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
+cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
 msbuild .\ALL_BUILD.vcxproj
-ctest -C "RelWithDebInfo"
+ctest -C "Release"
