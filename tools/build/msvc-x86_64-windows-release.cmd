ca7ade8ee28d86d2a0a955b1ca705dc7b625d8e1
方泓睿 <chfanghr@gmail.com>
2020-08-07 15:03:07 +0800

    ci: add msvc workflow (fix test config in build script)(try: 7)

--- tools/build/msvc-x86_64-windows-release.cmd
+++ tools/build/msvc-x86_64-windows-release.cmd
@@ -3,4 +3,4 @@ conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s buil
 CD cmake-msvc-x86_64-windows-build-release
 cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
 msbuild .\ALL_BUILD.vcxproj
-ctest -C "Debug"
+ctest -C "Release"
