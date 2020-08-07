d91d9a8c7299f22f80bd658a20af0715d7762b07
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2020-08-07 22:10:31 +0800

    ci: using conan build type MinSizeRel(try: 6)

--- tools/build_scripts/msvc-x86_64-windows-release.cmd
+++ tools/build_scripts/msvc-x86_64-windows-release.cmd
@@ -1,5 +1,5 @@
 MKDIR cmake-msvc-x86_64-windows-build-release
-conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s build_type=DebugAndRelease 
+conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s build_type=MinSizeRel
 CD cmake-msvc-x86_64-windows-build-release
 cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
 msbuild .\ALL_BUILD.vcxproj
