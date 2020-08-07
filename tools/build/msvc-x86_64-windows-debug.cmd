a7b63d036dd7996dda5a1797b14b6b54a37013dc
方泓睿 <chfanghr@gmail.com>
2020-08-07 14:29:00 +0800

    ci: add msvc workflow(build script added)(try: 2)

---
+++ tools/build/msvc-x86_64-windows-debug.cmd
@@ -0,0 +1,6 @@
+MKDIR cmake-msvc-x86_64-windows-build-debug
+conan install --install-folder=cmake-msvc-x86_64-windows-build-debug -s build_type=Debug
+CD cmake-msvc-x86_64-windows-build-debug
+cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
+msbuild .\ALL_BUILD.vcxproj
+ctest -C "Debug"
