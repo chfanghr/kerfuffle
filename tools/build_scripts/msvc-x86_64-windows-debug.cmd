0c06910446673f3ceddfc6001848db93b9c852b4
方泓睿 <chfanghr@gmail.com>
2020-08-07 19:10:23 +0800

    ci: fix build script(msvc debug build script)(try:2)

--- tools/build_scripts/msvc-x86_64-windows-debug.cmd
+++ tools/build_scripts/msvc-x86_64-windows-debug.cmd
@@ -1,5 +1,5 @@
 MKDIR cmake-msvc-x86_64-windows-build-debug
-conan install . --install-folder=cmake-msvc-x86_64-windows-build-debug build_type=Debug
+conan install . --install-folder=cmake-msvc-x86_64-windows-build-debug -s build_type=Debug
 CD cmake-msvc-x86_64-windows-build-debug
 cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
 msbuild .\ALL_BUILD.vcxproj
