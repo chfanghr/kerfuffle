b09ac8744cbf16cf5fe9840f8fe802863762fb3e
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2020-08-07 22:15:09 +0800

    ci: force rebuild(try: 7)

--- tools/build_scripts/msvc-x86_64-windows-release.cmd
+++ tools/build_scripts/msvc-x86_64-windows-release.cmd
@@ -1,5 +1,5 @@
 MKDIR cmake-msvc-x86_64-windows-build-release
-conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s build_type=Release
+conan install . --install-folder=cmake-msvc-x86_64-windows-build-release -s build_type=Release --build
 CD cmake-msvc-x86_64-windows-build-release
 cmake -G "Visual Studio 16 2019" -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -A x64 -v ..
 msbuild .\ALL_BUILD.vcxproj
