e98b3ccca39e679369efbb1b485f98da0086de7f
方泓睿 <chfanghr@gmail.com>
2020-08-07 23:16:57 +0800

    ci: add debug build script for linux ci(try: 1)

---
+++ tools/build_scripts/linux_debug_build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+BUILD_FOLDER="cmake-debug-build"
+CC="$(command -v clang-9)"
+CXX="$(command -v clang++-9)"
+
+mkdir ${BUILD_FOLDER}
+conan install . -s compiler=clang -s compiler.version=9 -s build_type=Debug --install-folder=${BUILD_FOLDER}
+
+cd ${BUILD_FOLDER}
+cmake -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -DENABLE_COVERAGE=OFF ..
+make -j8
+
+make test
