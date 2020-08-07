749936a9d13a919174a12af6d25603f5de07fadc
方泓睿 <chfanghr@gmail.com>
2020-08-07 23:46:28 +0800

    test: fix undefined stdlib symbol on linux(try: 2)

--- tools/build_scripts/linux_debug_build.sh
+++ tools/build_scripts/linux_debug_build.sh
@@ -10,7 +10,7 @@ mkdir ${BUILD_FOLDER}
 conan install . -s compiler=clang -s compiler.version=9 -s compiler.libcxx=libstdc++11 -s build_type=Debug --install-folder=${BUILD_FOLDER} --build
 
 cd ${BUILD_FOLDER}
-cmake -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -DENABLE_COVERAGE=OFF ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_EXAMPLES=ON -DENABLE_COVERAGE=OFF ..
 make -j8
 
 make test
